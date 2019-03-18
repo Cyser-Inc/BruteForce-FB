db28d1cf38ad1e1231b58cd466ba66ae77f3d5a3
Cyser Inc <39613273+Cyser-Inc@users.noreply.github.com>
2019-03-18 13:17:26 +0800

    Update BF.sh

--- BF.sh
+++ BF.sh
@@ -22,7 +22,7 @@ echo $i" |"$pu" YOUTUBE   :"$ku" Cyser Inc."$i"                      |"
 
 echo $i" |"$pu" WhatsApp  :"$ku" 0895802871229"$i"                   |"
 echo $i" |"$pu" Gmail     :"$ku" official.cyser@gmail.com"$i"        |"
-echo $i" |"$pu" THANKS TO :"$cy"~ [MR.N0T] [MR.M!ND$] [MR.€®®0®]"$i" |"
+echo $i" |"$pu" THANKS TO :"$cy" [MR.N0T] [MR.M!ND$] [MR.€®®0®]"$i"  |"
 
 echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo
@@ -260,8 +260,10 @@ echo ""
 sleep 2
 echo $i"Jangan Lupa Subscribe Channel Saya"
 sleep 1
-figlet "Cyser Inc."|lolcat
 echo $me"https://youtube.com/c/CyserInc"
+sleep 1
+figlet "Cyser Inc."|lolcat
+
 echo ""
 sleep 2
 echo $pur">> Thanks Yang Sudah Support Saya <<"
